70bbdcfa7ad3be0d118b8be782d7b03672d9d7ee
Chettimada, Vinayak <vinayak.kariappa.chettimada@nordicsemi.no>
2024-09-26 12:28:31 +0200

    Sync to broadcast audio source with delegator support

--- applications/nrf5340_audio/broadcast_sink/main.c
+++ applications/nrf5340_audio/broadcast_sink/main.c
@@ -542,7 +542,9 @@ int main(void)
 
 		ret = bt_mgmt_adv_start(0, ad, ARRAY_SIZE(ad), NULL, 0, true);
 		ERR_CHK(ret);
-	} else {
+	}
+
+	{
 		ret = bt_mgmt_scan_start(0, 0, BT_MGMT_SCAN_TYPE_BROADCAST,
 					 CONFIG_BT_AUDIO_BROADCAST_NAME, BRDCAST_ID_NOT_USED);
 		ERR_CHK_MSG(ret, "Failed to start scanning");
